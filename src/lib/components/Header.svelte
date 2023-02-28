325d309af2fdf2d00f4b0b7003980cacd7c35c69
Akiomi Kamakura <akiomik@gmail.com>
2023-02-28 23:09:08 +0900

    Add Header menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+  import { faBars } from '@fortawesome/free-solid-svg-icons';
   import { AppBar } from '@skeletonlabs/skeleton';
+  import { pubkey, seckey } from '$lib/stores/cookie';
+  import Menu from '$lib/components/Menu.svelte';
+
+  let showMenu = false;
+
+  const handleMenu = () => {
+    showMenu = !showMenu;
+  };
+
+  $: isLoggedIn = $pubkey !== '' && $seckey !== '';
 </script>
 
 <AppBar>
@@ -10,5 +22,18 @@
         Nosli
       </span>
     </a>
+  </svelte:fragment>
+  <svelte:fragment slot="trail">
+    {#if isLoggedIn}
+      <a href="/matome/new" class="btn bg-primary-500">Create</a>
+      <div class="relative">
+        <button class="btn-icon hover:variant-soft-surface" on:click={handleMenu}>
+          <FontAwesomeIcon icon={faBars} title="Open menu" />
+        </button>
+        <Menu show={showMenu} />
+      </div>
+    {:else}
+      <a href="/login" class="btn bg-primary-500">Login</a>
+    {/if}
   </svelte:fragment>
 </AppBar>
---
+++ src/lib/components/Menu.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { nip19 } from 'nostr-tools';
+  import { pubkey, seckey } from '$lib/stores/cookie';
+
+  export let show = false;
+
+  const logout = () => {
+    $pubkey = '';
+    $seckey = '';
+  };
+
+  $: isLoggedIn = $pubkey !== '' && $seckey !== '';
+</script>
+
+<nav class="card absolute w-48 p-4 right-0" class:hidden={!show}>
+  <ul class="flex flex-col space-y-4">
+    <li>
+      <a href="/p/${nip19.npubEncode($pubkey)}" class="inline-block w-full text-left">My page</a>
+    </li>
+    <li>
+      <button on:click={logout} class="w-full text-left">Logout</button>
+    </li>
+  </ul>
+</nav>
