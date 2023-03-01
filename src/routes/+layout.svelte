7eb929cddc6519301775639cbdc935dc054f3f44
Akiomi Kamakura <akiomik@gmail.com>
2023-03-01 22:50:30 +0900

    Add navigation loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,27 @@
-<script>
+<script lang="ts">
   import { AppShell } from '@skeletonlabs/skeleton';
   import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
   import '@skeletonlabs/skeleton/styles/all.css';
   import '@fortawesome/fontawesome-svg-core/styles.css';
+  import { beforeNavigate, afterNavigate } from '$app/navigation';
   import '../app.postcss';
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
+  import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
+
+  let loading = false;
+  let timeout: number;
+
+  beforeNavigate(() => {
+    timeout = setTimeout(() => {
+      loading = true;
+    }, 100);
+  });
+
+  afterNavigate(() => {
+    clearTimeout(timeout);
+    loading = false;
+  });
 </script>
 
 <AppShell>
@@ -14,7 +30,11 @@
   </svelte:fragment>
 
   <div class="container mx-auto p-4 space-y-8 mt-4">
-    <slot />
+    {#if loading}
+      <LoadingSpinner />
+    {:else}
+      <slot />
+    {/if}
   </div>
 
   <svelte:fragment slot="footer">
