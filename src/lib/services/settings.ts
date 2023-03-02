09fc351ce9ac7aa8ec5db6a85be1d615b5d874ac
Akiomi Kamakura <akiomik@gmail.com>
2023-03-03 01:00:23 +0900

    Add more relays

--- src/lib/services/settings.ts
+++ src/lib/services/settings.ts
@@ -1,8 +1,13 @@
 export const defaultRelays = [
-  'wss://relay.damus.io',
-  'wss://relay.snort.social',
+  'wss://brb.io',
+  'wss://no.str.cr',
   'wss://nos.lol',
-  'wss://relay.current.fyi',
+  'wss://nostr.bitcoiner.social',
   'wss://nostr.mom',
-  'wss://brb.io'
+  'wss://nostr.oxtr.dev',
+  'wss://nostr-pub.wellorder.net',
+  'wss://relay.current.fyi',
+  'wss://relay.damus.io',
+  'wss://relay.nostr.bg',
+  'wss://relay.snort.social'
 ];
