95bd84382e41dc8a71a935dffd9dadc61d9ea4d2
Akiomi Kamakura <akiomik@gmail.com>
2023-03-02 22:34:45 +0900

    Fix Alert

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
   export let variant = '';
-
-  $: className = variant === '' ? 'variant-ghost' : `variant-ghost-${variant}`;
-  // $: className = 'variant-ghost';
 </script>
 
-<!-- FIXME: This does not seems to work -->
-<!-- <aside class="alert {className}"> -->
-<aside class="alert variant-ghost">
+<aside class={variant === '' ? 'alert variant-ghost' : `alert variant-ghost-${variant}`}>
   <slot name="icon" />
 
   <div class="alert-message">
