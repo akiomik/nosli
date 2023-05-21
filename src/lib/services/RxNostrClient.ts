ca1afce222bd2ed14531639b0a1a56141caec320
Akiomi Kamakura <akiomik@gmail.com>
2023-05-22 08:35:33 +0900

    Remove unused methods

--- src/lib/services/RxNostrClient.ts
+++ src/lib/services/RxNostrClient.ts
@@ -1,8 +1,7 @@
 import { delay, flatMap, map, Observable } from 'rxjs';
 import type { RxNostr, EventPacket } from 'rx-nostr';
-import { RxBackwardReq, uniq, verify, latest, createRxNostr } from 'rx-nostr';
+import { RxBackwardReq, uniq, verify, createRxNostr } from 'rx-nostr';
 import { Kind } from 'nostr-tools';
-import NostrClient from '$lib/services/NostrClient';
 import Reaction from '$lib/entities/Reaction';
 
 export default class RxNostrClient {
@@ -33,90 +32,6 @@ export default class RxNostrClient {
     req.emit([{ kinds: [Kind.Text], ids }]);
 
     return this.rxNostr.use(req.pipe(delay(timeout))).pipe(uniq(), verify());
-  }
-
-  observableProfile({
-    pubkey,
-    timeout = 500
-  }: {
-    pubkey: string;
-    timeout?: number;
-  }): Observable<EventPacket> {
-    const req = new RxBackwardReq();
-    req.emit([
-      {
-        kinds: [Kind.Metadata],
-        authors: [pubkey],
-        limit: 1
-      }
-    ]);
-
-    return this.rxNostr.use(req.pipe(delay(timeout))).pipe(verify(), latest());
-  }
-
-  observableUserMatomes({
-    pubkey,
-    limit = 100,
-    timeout = 500
-  }: {
-    pubkey: string;
-    limit?: number;
-    timeout?: number;
-  }): Observable<EventPacket> {
-    const req = new RxBackwardReq();
-    req.emit([
-      {
-        kinds: [Kind.Article],
-        authors: [pubkey],
-        '#t': [NostrClient.TAG],
-        limit
-      }
-    ]);
-
-    // FIXME: To avoid duplication, make this unique by pubkey and indentifier
-    return this.rxNostr.use(req.pipe(delay(timeout))).pipe(uniq(), verify());
-  }
-
-  observableGlobalMatomes({
-    limit,
-    timeout = 500
-  }: {
-    limit: number;
-    timeout?: number;
-  }): Observable<EventPacket> {
-    const req = new RxBackwardReq();
-    req.emit([
-      {
-        kinds: [Kind.Article],
-        '#t': [NostrClient.TAG],
-        limit
-      }
-    ]);
-
-    return this.rxNostr.use(req.pipe(delay(timeout))).pipe(uniq(), verify());
-  }
-
-  observableMatome({
-    pubkey,
-    identifier,
-    timeout = 500
-  }: {
-    pubkey: string;
-    identifier: string;
-    timeout?: number;
-  }): Observable<EventPacket> {
-    const req = new RxBackwardReq();
-    req.emit([
-      {
-        kinds: [Kind.Article],
-        authors: [pubkey],
-        '#d': [identifier],
-        '#t': [NostrClient.TAG],
-        limit: 1
-      }
-    ]);
-
-    return this.rxNostr.use(req.pipe(delay(timeout))).pipe(verify(), latest());
   }
 
   observableReactions({
